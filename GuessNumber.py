00c406542856faf81c5064a27df9d2d401c1de98
Toastling <ca.el.ril@gmail.com>
2019-01-31 12:13:16 -0800

    ProgramwithBool

--- GuessNumber.py
+++ GuessNumber.py
@@ -1,4 +1,50 @@
 #!/usr/bin/env python
 
-for fruit in ['banana', 'grape', 'pear', 'kiwi']:
-    print "I like to eat "+ fruit+'s'
+import random
+
+# sides = raw_input("You have a die! How many sides does it have?") 
+# numrolls = raw_input("How many times would you like to roll?")
+# type(sides)
+# type(numrolls)
+# print "Rolling die... Your results are:"
+# i = 0
+# total = 0
+# while i < int(numrolls):
+#     currentroll = random.randint(1, int(sides))
+#     total = total + currentroll
+#     print currentroll
+#     i = i + 1
+# t = raw_input("Would you like a total?")
+# type(t) 
+# if t.strip() == "Yes":
+#     print total
+# else: print ("Okay. Bye. Dumb bitch :)")
+
+# NumberRange = raw_input("I'm thinking of a number between 1 and 10. Guess the number.") 
+# numrolls = raw_input("How many times would you like to roll?")
+# print "Rolling die... Your results are:"
+# i = 0
+
+#     currentroll = random.randint(1, int(10))
+#     return currentroll
+#     i = i + 1
+# t = raw_input("Would you like a total?")
+# type(t) 
+# if t.strip() == "Yes":
+#     print total
+# else: print ("Okay. Bye. Dumb bitch :)")
+
+
+
+
+currentroll = random.randint(1, int(10))
+MadeCorrectGuess=False
+while not MadeCorrectGuess:
+    NumberRange = raw_input("I'm thinking of a number between 1 and 10. Guess the number.") 
+    if int(NumberRange)== currentroll:
+        MadeCorrectGuess=True
+        print "Yes. That is correct."
+    elif int(NumberRange)<currentroll:
+        print "Nope. Go bigger."
+    elif int(NumberRange)>currentroll:
+        print "Nope. Go smaller."
