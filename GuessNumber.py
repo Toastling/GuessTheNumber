a5b8d22e097a1987e62aea8e56dc14546c8a8527
Carter Currin <ccurrin@kochava.com>
2019-01-31 12:21:18 -0800

    Version that does not use the extra bollean variable but still works. It can be considered less reable though.

--- GuessNumber.py
+++ GuessNumber.py
@@ -38,13 +38,12 @@ import random
 
 
 currentroll = random.randint(1, int(10))
-MadeCorrectGuess=False
-while not MadeCorrectGuess:
+NumberRange=-1
+while int(NumberRange) != currentroll:
     NumberRange = raw_input("I'm thinking of a number between 1 and 10. Guess the number.") 
-    if int(NumberRange)== currentroll:
-        MadeCorrectGuess=True
-        print "Yes. That is correct."
-    elif int(NumberRange)<currentroll:
+    if int(NumberRange) < currentroll:
         print "Nope. Go bigger."
-    elif int(NumberRange)>currentroll:
+    else:
         print "Nope. Go smaller."
+
+print "Yes. That is correct."
